84467edd027b6d62cd87cc0e60e3c0786b88290c
KristinaHejlesen <kkhejlesen@gmail.com>
2025-12-16 08:04:46 +0100

    Tilføjet html til navigation+javascript(ikke færdig)
    Mangler stadig noget CSS og JS for at navigationen er færdig

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,1 +1,11 @@
 
+  /*burgermenu */
+ 
+  const burger = document.querySelector("#menu");
+ 
+  const mobileMenu = document.querySelector(".globalNav");
+
+ /*når der klikkes på menu ikonen, tilføjes classen aktiv og nav indholdet vises. */
+  burger.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+  });
