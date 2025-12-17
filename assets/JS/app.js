c95b86c2a6af137c85fc4ed9fb656f690e5e2c25
KristinaHejlesen <kkhejlesen@gmail.com>
2025-12-17 09:17:37 +0100

    tilføjet styles til nav

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -8,6 +8,8 @@
  /*når der klikkes på menu ikonen, tilføjes classen aktiv og nav indholdet vises. */
   burger.addEventListener("click", () => {
     mobileMenu.classList.toggle("active");
+    /*tilføjer class active til burgermenu ikonet, dette bruges til at lave animationen fra tre streger til et kryds */
+    burger.classList.toggle("active");
   });
 
   /*dropdown klik */
@@ -15,12 +17,21 @@
   const btnNav = document.querySelectorAll(".dropbtn");
   
   /*looper igennem alle knapper med class dropbtn og lytter efter click.  */
-  btnNav.forEach((btn) => {
+  btnNav.forEach((btn, i) => {
     btn.addEventListener("click", () => {
       /*nextElementSibling returne det næste element, som i dette tilfælde er vores dropdownIndhold, der tilføjer vi class show */
       btn.nextElementSibling.classList.toggle("show");
     });
   });
   
+  /*dropdown animation.. virker ikke!!! OBS nodelist.. skal loopes igennem!*/
+  const ikon = document.querySelectorAll(".animatepil");
+  ikon.forEach((ikon) =>{
+    btn.addEventListener("click", () =>{
+      ikon.classList.toggle("dropdownAnimation");
+
+    });
+
+  });
 
  
