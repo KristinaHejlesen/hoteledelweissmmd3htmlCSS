9585e82ec8445cd7e0fe7e7a2a2ea6dd1c12fb7e
KristinaHejlesen <kkhejlesen@gmail.com>
2025-12-27 14:09:35 +0100

    ændret button hover

---
+++ assets/JS/splidejs.js
@@ -0,0 +1,6 @@
+/*splider slider */
+ new Splide( '.splide' ).mount();
+  document.addEventListener( 'DOMContentLoaded', function() {
+    const splide = new Splide( '.splide' );
+    splide.mount();
+  } );
